a47f38f19aed4e4b9af33941cbdc78ceb3474f26
Andrew Schreiber <aschreiber@VA00029-PC.local>
2015-06-15 14:12:09 -0400

    Add automatic deserialization to models if you specify {embedded:true} in the attribute definition. Previously this required building a serializer per model and created a fair bit of boiler plate code.
    Also added repository to package.json and moved to version 0.0.1

--- addon/serializers/application.js
+++ addon/serializers/application.js
@@ -3,6 +3,16 @@ import DS from 'ember-data';
 
 export default DS.JSONSerializer.extend(DS.EmbeddedRecordsMixin, {
 
+  normalize: function(typeClass, hash, prop){
+    hash.id = hash.id || Ember.generateGuid({}, typeClass.typeKey);
+    // var normalizedHash = this._super(typeClass, hash, prop);
+    return extractEmbeddedRecords(this, this.store, typeClass, hash);
+  },
+  hasEmbeddedOption: function(type, attr){
+    var meta = type.metaForProperty(attr)
+    return meta && meta.options.embedded
+  },
+
   serializeIntoHash: function(hash, type, record, options){
     Ember.merge(hash, this.serialize(record, options));
   },
@@ -23,11 +33,59 @@ export default DS.JSONSerializer.extend(DS.EmbeddedRecordsMixin, {
 
     payload.id = payload.id || id || Ember.generateGuid({}, type.typeKey);
     return this._super(store, type, payload, id, requestType)
-  },
-
-  normalize: function(type, hash, prop){
-    hash.id = hash.id || Ember.generateGuid({}, type.typeKey);
-    return this._super(type, hash, prop);
-
   }
+
 })
+
+// chooses a relationship kind to branch which function is used to update payload
+// does not change payload if attr is not embedded
+function extractEmbeddedRecords(serializer, store, typeClass, partial) {
+
+  typeClass.eachRelationship(function(key, relationship) {
+    if (serializer.hasDeserializeRecordsOption(key) || serializer.hasEmbeddedOption(typeClass, key)) {
+      var embeddedTypeClass = store.modelFor(relationship.type);
+      if (relationship.kind === "hasMany") {
+        extractEmbeddedHasMany(store, key, embeddedTypeClass, partial);
+      }
+      if (relationship.kind === "belongsTo") {
+        extractEmbeddedBelongsTo(store, key, embeddedTypeClass, partial);
+      }
+    }
+  });
+
+  return partial;
+}
+
+// handles embedding for `hasMany` relationship
+function extractEmbeddedHasMany(store, key, embeddedTypeClass, hash) {
+  if (!hash[key]) {
+    return hash;
+  }
+
+  var ids = [];
+
+  var embeddedSerializer = store.serializerFor(embeddedTypeClass.typeKey);
+  hash[key].forEach(function(data) {
+    var embeddedRecord = embeddedSerializer.normalize(embeddedTypeClass, data, null);
+    store.push(embeddedTypeClass.typeKey, embeddedRecord);
+    ids.push(embeddedRecord.id);
+  });
+
+  hash[key] = ids;
+  return hash;
+}
+
+
+function extractEmbeddedBelongsTo(store, key, embeddedTypeClass, hash) {
+  if (!hash[key]) {
+    return hash;
+  }
+
+  var embeddedSerializer = store.serializerFor(embeddedTypeClass.typeKey);
+  var embeddedRecord = embeddedSerializer.normalize(embeddedTypeClass, hash[key], null);
+  store.push(embeddedTypeClass.typeKey, embeddedRecord);
+
+  hash[key] = embeddedRecord.id;
+  //TODO Need to add a reference to the parent later so relationship works between both `belongsTo` records
+  return hash;
+}
